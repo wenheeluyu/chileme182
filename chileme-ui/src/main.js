77b47683ad74c0977d95834bc28152af2e15ba4b
wenheeluyu <302505253@qq.com>
2020-04-23 16:17:33 +0800

    配置前端项目

---
+++ chileme-ui/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Axios from './axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.config.productionTip = false
+
+Vue.prototype.Axios = Axios   //配置axios
+Vue.use(ElementUI)  //配置element-ui
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ chileme-ui/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+  div.home
+    img(alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App")
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
