3e85d4a77619a609bb62732271f8b4bcdb81dbcd
wenheeluyu <302505253@qq.com>
2020-04-27 21:54:05 +0800

    链接mongodb

--- chileme-sev/app.js
+++ chileme-sev/app.js
@@ -1,19 +1,25 @@
 const Koa = require('koa')
-
 const app = new Koa()
+const cfg = require('./config') // 引入配置文件
+const mongoose = require('mongoose') //引入mongoose
+const KoaBody = require('koa-body') //解析post请求
+const router = require('./router/router') // 引入路由(接口)
+// 配置跨域 
+const KoaCors = require('koa-cors') //引入跨域模块
+app.use(KoaCors({
+    credentials:true,
+    origin:'http:127.0.0.1:8080'
+}))  //配置跨域模块
 
-
-const KoaBody = require('koa-body')  //解析post请求
-
-app.use(async ctx=>{
-    ctx.body = 'helloooooooo'
+// 配置数据库链接
+mongoose.connect(cfg.path)
+mongoose.connection.on('error',()=>{
+    console.log('数据库链接错误')
 })
-app.use(KoaBody())     //配置Koa-Body
-// const Router = require('koa-router')
-// const router = new Router()
-// app
-//   .use(router.routes())
-//   .use(router.allowedMethods())
-
-app.listen(3000)
-console.log(`server is running at http://127.0.0.1:3000`)
+mongoose.connection.once('open',() => {
+    console.log('数据库已连接')
+})
+app.use(KoaBody()) // 配置koa-body
+app.use(router.routes()).use(router.allowedMethods()) // 配置路由
+app.listen(cfg.port)
+console.log(`server is running at http://127.0.0.1:${cfg.port}`)
