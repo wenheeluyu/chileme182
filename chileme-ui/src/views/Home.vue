6525666529e9dff853902eff265e21cf637eaf87
wenheeluyu <302505253@qq.com>
2020-04-29 20:23:49 +0800

    注册页面切图（无交互）

--- chileme-ui/src/views/Home.vue
+++ chileme-ui/src/views/Home.vue
@@ -4,25 +4,25 @@
           el-col(:span='1')
               div.gridlt
                 div.menuBtn(@click="jumpTo('/goods')")
-                   span.iconfont &#xe662;
+                   span.iconfont &#xe689;
                    i 收银
                 div.menuBtn
-                   span.iconfont &#xe53d;
+                   span.iconfont &#xe653;
                    i 会员  
                 div.menuBtn(@click="jumpTo('/stockManagement')")
-                   span.iconfont &#xe65d;
+                   span.iconfont &#xe50e;
                    i 库存
                 div.menuBtn
-                   span.iconfont &#xe635;
+                   span.iconfont &#xe6f6;
                    i 换班
                 div.menuBtn
-                   span.iconfont &#xe63f;
+                   span.iconfont &#xe683;
                    i 报表
                 div.menuBtn
-                   span.iconfont &#xe65e;
+                   span.iconfont &#xe6da;
                    i 辅助
                 div.menuBtn
-                   span.iconfont &#xe602;
+                   span.iconfont &#xe60d;
                    i 退出                
           el-col(:span='23')
               div.gridrt
@@ -34,13 +34,13 @@ export default {
   data(){
     return{
       ltBtn:[
-        {id:1,icon:'&#xe662;',name:'收银',link:''},
-        {id:2,icon:'&#xe53d;',name:'会员',link:''},
-        {id:3,icon:'&#xe65d;',name:'库存',link:''},
-        {id:4,icon:'&#xe635;',name:'换班',link:''},
-        {id:5,icon:'&#xe63f;',name:'报表',link:''},
-        {id:6,icon:'&#xe65e;',name:'辅助',link:''},
-        {id:7,icon:'&#xe602;',name:'退出',link:''},
+        {id:1,icon:'&#xe689;',name:'收银',link:''},
+        {id:2,icon:'&#xe653;',name:'会员',link:''},
+        {id:3,icon:'&#xe50e;',name:'库存',link:''},
+        {id:4,icon:'&#xe6f6;',name:'换班',link:''},
+        {id:5,icon:'&#xe683;',name:'报表',link:''},
+        {id:6,icon:'&#xe6da;',name:'辅助',link:''},
+        {id:7,icon:'&#xe60d;',name:'退出',link:''},
       ]
     }
   },
--- chileme-ui/src/views/Register.vue
+++ chileme-ui/src/views/Register.vue
@@ -1,6 +1,21 @@
 <template lang="pug">
     div.register
-        h1 我是注册页面
+        div.registerBox
+            div.icon.el-icon-food
+            el-form(:model="form" :rules="rules" ref="Form" label-width="100px")
+                el-form-item(label="用户名" prop="username")
+                    el-input(v-model="form.username")
+                el-form-item(label="密码" prop="possword")
+                    el-input(type='possword' v-model="form.possword")
+                el-form-item(label="确认密码" prop="cfm")
+                    el-input(type='possword' v-model="form.cfm")
+                el-form-item(label="手机号" prop="moblie")
+                    el-input(v-model="form.moblie")
+                el-form-item
+                    el-button(type="primary" @click="registerHandle('Form')") 立即注册
+                    el-button(@click="resetHandle('Form')") 重置
+            el-link(@click='jumpLogin') 点击登录   
+            
 </template>
 <script>
 export default {
@@ -11,18 +26,77 @@ export default {
                 psssword:'',
                 cfm:'',
                 moblie:''
+            },
+            //表单验证规则
+            rules:{
+                username:[
+                    { required: true, message: '请输入用户名称', trigger: 'blur' },
+                    // { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+                ],
+                 psssword:[
+                    { required: true, message: '请输入密码', trigger: 'blur' },
+                    // { min: 6, max: 20, message: '长度在 6 到 20 个字符', trigger: 'blur' }
+                ],
+                cfm:[
+                    { required: true, message: '请输入密码', trigger: 'blur' },
+
+                ],
+                moblie:[
+                    { required: true, message: '请输入手机号', trigger: 'blur' },
+                ]
+                
             }
         }
     },
     methods:{
         //注册
-        registerHandle(){
-
+        registerHandle(formName){
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    alert('注册成功!');
+                } else {
+                    console.log('注册失败!!');
+                    return false;
+                }
+            });
         },
         //重置
-        resetHandle(){
-            
+        resetHandle(formName){
+            this.$refs[formName].resetFields();
+        },
+        //立即注册
+        jumplogin(){
+            alert('跳转登录页')
+            //this.$router.push()
         }
     }
 }
 </script>
+<style lang="scss" scoped>
+.register{
+    height: 100%;
+    background:  rgb(226, 226, 226);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .registerBox{
+        width: 800px;
+        height: 650px;
+      
+        .icon{
+            width: 150px;
+            height: 150px;
+            line-height: 150px;
+            font-size: 120px;
+            background: rgb(250, 178, 23);
+            color: #fff;
+            border-radius: 50%;
+           
+        }
+        .el-form{
+            width: 450px;
+            margin: 30px auto 10px;
+        }
+    }
+}
+</style>
