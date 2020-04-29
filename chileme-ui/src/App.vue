f031cf6a8ccf7ce05d7ef75997a36d4fe79d0dfa
wenheeluyu <302505253@qq.com>
2020-04-29 16:37:15 +0800

    项目结构修改

--- chileme-ui/src/App.vue
+++ chileme-ui/src/App.vue
@@ -1,31 +1,35 @@
 <template lang='pug'>
   div#app
-    div#nav
-      router-link(to="/") Home |
-      router-link(to="/about") About
-    router-view
-
+      router-view
 </template>
-
+<script>
+export default {
+  data(){
+    return{
+  
+    }
+  }
+}
+</script>
 <style lang="scss">
+@mixin init {
+  width:100%;
+  height: 100%;
+  overflow: hidden;
+  text-align: center;
+}
+@mixin flx($dec:column){
+  display: flex;
+  flex-direction:$dec;
+}
+html,body{
+  @include init;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  @include init;
+  background: slateblue;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</style>s
